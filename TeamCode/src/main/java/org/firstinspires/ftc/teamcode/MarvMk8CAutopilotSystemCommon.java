87c51b7457f13b37782d789f3a0087f4c9e964e2
Aedan Cullen <aedancullen@gmail.com>
2017-12-06 13:13:26 -0500

    Add skeleton onSegmentTransition body

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MarvMk8CAutopilotSystemCommon.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MarvMk8CAutopilotSystemCommon.java
@@ -16,7 +16,25 @@ public class MarvMk8CAutopilotSystemCommon extends AutopilotSystem {
     }
 
     public void onSegmentTransition(AutopilotSegment previous, AutopilotSegment next, boolean wasOkayToContinue) {
-
+        
+        if (next.id.equals("__start__")){
+            // Do vision sensing, drive off balancing stone and set angle hold
+        }
+        else if (previous.id.equals("approach_crypto")){
+            // Approach and glyph ejection routine
+            // Can use 
+            //
+            // this.host.setNavigationTarget(AutopilotSegment);
+            // this.host.setNavigationStatus(NavigationStatus.RUNNING);
+            // while (this.host.getNavigationStatus == NavigationStatus.RUNNING) {
+            //      double[2] power = this.host.navigationTickRaw();
+            //      // marv.drive stuff
+            // }
+            // // marv.drive stop the robot
+            //
+            // to get to arbitrary coords for glyph ejection
+            // Then can drop glyph and proceed
+        }
     }
 
 }
