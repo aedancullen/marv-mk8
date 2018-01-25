1e2e4e04c0b51feda22cb0a64d05a3cb96e39e83
Aedan Cullen <aedancullen@gmail.com>
2018-01-25 09:47:30 -0500

    Update MarvNavConstants.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MarvNavConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MarvNavConstants.java
@@ -9,5 +9,7 @@ public class MarvNavConstants {
     public static double MbXOffset = 7;
 
     public static double MbYOffset = 2;
+    
+    public static double CryptXOffset = 5; // set appropriately!
 
 }
