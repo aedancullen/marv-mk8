a7bacd2714e9b0617c30e7e3627c11ccd61b2e58
Aedan Cullen <aedancullen@gmail.com>
2018-03-02 22:44:55 -0500

    Autonomous switch to MaxBotix free-run filtered mode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MarvMk8CAutoRedA.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MarvMk8CAutoRedA.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.evolutionftc.autopilot.AutopilotSystem;
 import com.evolutionftc.autopilot.AutopilotTrackerMso;
+import com.evolutionftc.autopilot.AutopilotTrackerMsoSimple;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
@@ -16,14 +17,14 @@ public class MarvMk8CAutoRedA extends LinearOpMode {
     MarvMk8CCommon marv;
     MarvMk8CAutopilotSystemCommonV2 marvAuto;
     
-    AutopilotTrackerMso mbxTracker;
+    AutopilotTrackerMsoSimple mbxTracker;
 
     public void runOpMode() {
          marv = new MarvMk8CCommon(hardwareMap);
         marv.isOnRedSide = true;
         marv.isOnBSide = false;
 
-        mbxTracker = new AutopilotTrackerMso(marv.sonarRx, marv.sonarL, marv.sonarB, MarvNavConstants.MbXOffset, MarvNavConstants.MbYOffset);
+        mbxTracker = new AutopilotTrackerMsoSimple(marv.sonarRx, marv.sonarL, marv.sonarB, MarvNavConstants.MbXOffset, MarvNavConstants.MbYOffset);
         
         marvAuto = new MarvMk8CAutopilotSystemCommonV2(this, mbxTracker, telemetry, hardwareMap.appContext);
         marvAuto.setMarvCommon(marv);
